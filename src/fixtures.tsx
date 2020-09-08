97a5a61c7f342ad5ee05401f4330b2b64ed1e626
kimdhamilton <kimdhamilton@gmail.com>
2020-09-07 20:52:06 -0700

    add cer example

--- src/fixtures.tsx
+++ src/fixtures.tsx
@@ -2,6 +2,7 @@ import degree from './samples/degree.json';
 import openBadge from './samples/openBadge.json';
 import openSkillsAssertion from './samples/openSkillsAssertion.json';
 import courseCertificate from './samples/courseCertificate.json';
+import credentialEngine from './samples/credentialEngine.json';
 import didDoc from './samples/didDoc.json';
 
 export const smallList = [
@@ -20,7 +21,11 @@ export const smallList = [
   {
     name: 'Degree',
     document: degree
-  }
+  },
+  {
+    name: 'Credential Engine Example',
+    document: credentialEngine
+  }  
 ];
 
 export const didDocument = didDoc;
