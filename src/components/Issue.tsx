5ffe2956202b92f5c0b051c52f324180dab181ea
Artem <arte.tkachuk@gmail.com>
2020-09-22 09:10:31 +0300

    added UI and refactor of API

--- src/components/Issue.tsx
+++ src/components/Issue.tsx
@@ -1,57 +1,94 @@
 import React, { FC, useState } from 'react';
-
-import Typography from '@material-ui/core/Typography';
+import COLORS from '../utils/colors';
 import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
 import { SigningProps } from './Props';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
 import { JSONEditor } from '@material-did/common';
 import { didDocument } from '../fixtures';
+import styled from 'styled-components';
 import { CircularProgress } from '@material-ui/core';
+import { SignedDocumentRequest } from './../api/index';
+
 const fetch = require('node-fetch');
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: Segoe UI;
+  font-size: 25px;
+  color: ${COLORS.METEORITE};
+  margin-top: 20px;
+`;
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-      '& .MuiTextField-root': {
-        margin: theme.spacing(1),
-        width: '25ch',
-      },
-    },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-  }),
-);
+const Content = styled.div`
+  display: flex;
+  margin-top: 20px;
+`;
+
+const Title = styled.div`
+  font-size: 2em;
+`;
+
+const Form = styled.form`
+  flex-grow: 1 1 auto;
+  flex-basis: 50%;
+  padding: 10px;
+`;
+
+const SubTitle = styled.div`
+  font-style: italic;
+  font-size: 1.6em;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+
+const ContainerDidDoc = styled.div`
+  font-size: 0.3em;
+  color: ${COLORS.METEORITE};
+`;
+
+const Button = styled.button`
+  font-size: 0.8em;
+  height: 2em;
+  border: none;
+  border-radius: 10px;
+  color: ${COLORS.METEORITE};
+  margin-top: 2em;
+  background: #f8f8f8;
+  box-shadow: -10px 10px 20px rgba(211, 211, 211, 0.2),
+    10px -10px 20px rgba(211, 211, 211, 0.2),
+    -10px -10px 20px rgba(255, 255, 255, 0.9),
+    10px 10px 25px rgba(211, 211, 211, 0.9);
+`;
+
+const ContainerCredential = styled.div`
+  flex-grow: 1 2 auto;
+  flex-basis: 50%;
+  padding: 10px;
+`;
+
+const ContainerEditor = styled.div`
+  overflow: hidden;
+  border: 10px solid #4827a7;
+  border-radius: 20px;
+  height: 500px;
+`;
 
 export const Issue: FC<SigningProps> = ({
-  document, setDocument, signedDocument, setSignedDocument
+  document,
+  setDocument,
+  signedDocument,
+  setSignedDocument,
 }) => {
-  const classes = useStyles();
-
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
     setLoading(true);
     try {
-      const response = await fetch('https://sign-and-verify.herokuapp.com/issue/credentials', {
-        method: 'POST',
-        body: JSON.stringify(document),
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: "application/json"
-        }
+      const response = await SignedDocumentRequest({
+        source: JSON.stringify(document),
       });
-      if (response.status !== 201) {
-        throw new Error(response.statusText);
-      }
-      const signedDocument = await response.json();
+      const signedDocument = response.data;
       setSignedDocument(signedDocument);
     } catch (error) {
       console.log(error);
@@ -61,40 +98,35 @@ export const Issue: FC<SigningProps> = ({
   };
 
   return (
-    <div className={classes.root}>
-      <Typography variant="h4" color="primary" gutterBottom>Signing Demonstration (test only)</Typography>
-      <form className={classes.root} noValidate autoComplete="off" onSubmit={handleSubmit}>
-        <div>
-          <Grid container spacing={3} justify="space-around">
-            <Grid item xs={6}>
-              <Typography variant="h6" color="primary" gutterBottom>Assertion Method</Typography>
-              <TextField label="Assertion Method" value={didDocument.publicKey[0].id} style={{ width: 500 }} />
-              <br />
-              <Button variant="contained" color="primary" type="submit" >
-                Sign Credential
-              </Button>
-              <div>
-              { loading &&
-                <CircularProgress variant="indeterminate" />
-               }
-               </div>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography variant="h6" color="primary" gutterBottom>Credential</Typography>
-              <JSONEditor value={JSON.stringify(document, null, 2)}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <Typography variant="h6" color="primary" gutterBottom>Signed Credential</Typography>
-              <JSONEditor value={signedDocument ? JSON.stringify(signedDocument, null, 2) : '{}'} />
-            </Grid>
-          </Grid>
-        </div>
-      </form>
-    </div >
+    <Container>
+      <Title>Signing Demonstration (test only)</Title>
+      <Content>
+        <Form noValidate autoComplete="off" onSubmit={handleSubmit}>
+          <SubTitle> Assertion Method</SubTitle>
+          <ContainerDidDoc>
+            <TextField
+              value={didDocument.publicKey[0].id}
+              style={{ width: 400 }}
+            />
+          </ContainerDidDoc>
+          <Button type="submit">Sign credential</Button>
+          <div>{loading && <CircularProgress variant="indeterminate" />}</div>
+        </Form>
+        <ContainerCredential>
+          <SubTitle>Credential</SubTitle>
+          <ContainerEditor>
+            <JSONEditor value={JSON.stringify(document, null, 2)} />
+          </ContainerEditor>
+        </ContainerCredential>
+      </Content>
+      <SubTitle>Signed Credential</SubTitle>
+      <ContainerEditor>
+        <JSONEditor
+          value={
+            signedDocument ? JSON.stringify(signedDocument, null, 2) : '{}'
+          }
+        />
+      </ContainerEditor>
+    </Container>
   );
 };
-
-
-
-
