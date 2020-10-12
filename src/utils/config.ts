2c4fda36f2e12a612e4d8b91d23e505b7d262bcd
kimdhamilton <kimdhamilton@gmail.com>
2020-10-12 12:08:57 -0700

    cleanup

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -10,7 +10,7 @@ let CONFIG: null | Config = null;
 
 export function parseConfig(): Config {
   return Object.freeze({
-    signAndVerifyEndpoint: process.env.SIGN_AND_VERIFY_ENDPOINT ? process.env.SIGN_AND_VERIFY_ENDPOINT  : 'http://127.0.0.1:5000',
+    signAndVerifyEndpoint: process.env.SIGN_AND_VERIFY_ENDPOINT ? process.env.SIGN_AND_VERIFY_ENDPOINT  : 'https://sign-and-verify.herokuapp.com',
     signingKeyId: didDocument.publicKey[0].id
   });
 }
