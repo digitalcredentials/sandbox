c16d24d4fc3902d7431b21a687c5f4618a849f91
kimdhamilton <kimdhamilton@gmail.com>
2020-11-12 16:04:43 -0800

    update samples

--- src/components/CredentialForm.tsx
+++ src/components/CredentialForm.tsx
@@ -4,6 +4,7 @@ import { CircularProgress } from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
 import COLORS from "../utils/colors";
 import { getConfig } from "../utils/config";
+import { SubTitle, Button } from "../utils/styles";
 
 const CONFIG = getConfig();
 
@@ -25,26 +26,6 @@ const Form = styled.form`
 const ContainerDidDoc = styled.div`
   font-size: 0.3em;
   color: ${COLORS.METEORITE};
-`;
-
-const SubTitle = styled.div`
-  font-size: 1.1em;
-  font-weight: bold;
-  margin-bottom: 20px;
-`;
-
-const Button = styled.button`
-  font-size: 0.8em;
-  height: 2em;
-  border: none;
-  border-radius: 10px;
-  color: ${COLORS.METEORITE};
-  margin-top: 2em;
-  background: ${COLORS.ALABASTER};
-  box-shadow: -10px 10px 20px rgba(211, 211, 211, 0.2),
-    10px -10px 20px rgba(211, 211, 211, 0.2),
-    -10px -10px 20px rgba(255, 255, 255, 0.9),
-    10px 10px 25px rgba(211, 211, 211, 0.9);
 `;
 
 
