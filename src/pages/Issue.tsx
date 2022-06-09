36a1f88818b379a5eaf7efd770ef38075dfb52a9
Gabe Fields <20584379+fields37@users.noreply.github.com>
2022-06-09 19:53:23 -0400

    Rough draft of Issue page UI, non functional

--- src/pages/Issue.tsx
+++ src/pages/Issue.tsx
@@ -5,14 +5,21 @@ import { Credential, CredentialEditor, CredentialForm } from "../components";
 import { getConfig } from "../utils/config";
 import { Title, Content, Container } from  "../utils/styles";
 
+import { JSONEditor } from "@material-did/common";
+import Button from "@material-ui/core/Button";
+import { encodeToQrCodeUrl, encodeToVpUnsigned } from "../utils/codecs";
+import { ProvePresentationRequest } from "../api/index";
+
 const CONFIG = getConfig();
 
 export const Issue: FC<SigningProps> = ({
   document,
+  setDocument,
   signedDocument,
   setSignedDocument,
 }) => {
   const [loading, setLoading] = useState(false);
+  const [qrCodeUrl, setQrCodeUrl] = useState('');
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
@@ -27,21 +34,44 @@ export const Issue: FC<SigningProps> = ({
       setLoading(false);
     }
   };
+  
+  const editorOnChange = async (data: string) => {
+    try {
+      const dataJson = JSON.parse(data);
+      setDocument(dataJson);
+    } catch (error) {}
+  };
+
+  const updateQrCodeUrl = async (document: any) => {
+    const vpUnsigned = encodeToVpUnsigned(document);
+    const vpSigned = (await ProvePresentationRequest(vpUnsigned)).data;
+    const qrCodeUrl = await encodeToQrCodeUrl(vpSigned);
+    setQrCodeUrl(qrCodeUrl);
+  };
 
   return (
     <Container>
-      <Title>Issue Credential</Title>
+      {/* <Title>Issue Credential</Title> */}
       <Content>
+        <JSONEditor
+          value={JSON.stringify(document, null, 2)}
+          onChange={editorOnChange}
+        />
+
         <CredentialForm handleSubmit={handleSubmit} loading={loading} buttonText={'Sign Credential'} subtitleText={'Assertion Method'} initialValue={CONFIG.signingKeyId}/>
-        <Credential
+        {/* <Credential
           subTitle="Credential"
           value={JSON.stringify(document, null, 2)}
-        />
+        /> */}
       </Content>
+      <Button onClick={handleSubmit} variant="contained" size="large" color="primary">Issue Credential</Button>
       <CredentialEditor
         subTitle="Signed Credential"
         value={signedDocument ? JSON.stringify(signedDocument, null, 2) : "{}"}
       />
+      <Button color="primary">
+        Verify this Credential
+      </Button>
     </Container>
   );
 };
