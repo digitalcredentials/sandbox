fd4a2d1c9967ebfb53fa300986878870be212e5d
Gabe Fields <20584379+fields37@users.noreply.github.com>
2022-06-23 11:15:32 -0400

    Changed CredentialEditor to CredentialViewer

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,5 @@
 export * from "./Credential";
-export * from "./CredentialEditor";
+export * from "./CredentialViewer";
 export * from "./IssueForm";
 export * from "./VerifyForm";
 export * from "./Props";
