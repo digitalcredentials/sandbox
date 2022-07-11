a1acdc827655e9a31774b0704b896248a174631f
Gabe Fields <20584379+fields37@users.noreply.github.com>
2022-07-11 15:44:29 -0400

    Added copy to clipboard button to signed credential

--- src/components/IssueForm.tsx
+++ src/components/IssueForm.tsx
@@ -66,8 +66,15 @@ export const IssueForm = ({ loading, handleSubmit, formState, setOptions}: Props
   }
 
   return (
+    // Issue Parameters Form
     <Form noValidate autoComplete="off" onSubmit={handleSubmit}>
-      <Box  sx={{alignContent: "center"}} display="flex" alignItems="flex-start" flexDirection="column">
+      <Box
+        sx={{alignContent: "center"}}
+        display="flex"
+        alignItems="flex-start"
+        flexDirection="column"
+      >
+
         <FormControl>
           <FormLabel className="formLabel">Did Method</FormLabel>
           <Select
@@ -81,21 +88,34 @@ export const IssueForm = ({ loading, handleSubmit, formState, setOptions}: Props
         </FormControl>
 
         <FormControl margin="none">
-          {/* DID selection */}
           <FormLabel>Sign With</FormLabel>
           <RadioGroup
             name="randomDid"
             onChange={handleChange}
             value={formState.randomDid.toString()}
           >
-            <FormControlLabel value="true" control={<Radio />} label="New Random DID" />
-            <FormControlLabel value="false" control={<Radio />} label="Existing DID" />
+            <FormControlLabel
+              value="true"
+              control={<Radio />}
+              label="New Random DID"
+            />
+            <FormControlLabel
+              value="false"
+              control={<Radio />}
+              label="Existing DID"
+            />
           </RadioGroup>
         </FormControl>
 
         <FormControl>
-            {/* <FormLabel className="formLabel">Did Seed</FormLabel> */}
-            <TextField name="didSeed" value={formState.didSeed} id="standard-basic" label="Secret key seed" variant="standard" onChange={handleChange} />
+            <TextField
+              name="didSeed"
+              value={formState.didSeed}
+              id="standard-basic"
+              label="Secret key seed"
+              variant="standard"
+              onChange={handleChange}
+            />
         </FormControl>
 
         <FormControl>
@@ -105,20 +125,30 @@ export const IssueForm = ({ loading, handleSubmit, formState, setOptions}: Props
             value={formState.keySuite}
             onChange={handleChange}
           >
-            <MenuItem value={"Ed25519Signature2020"}>Ed25519Signature2020</MenuItem>
-            <MenuItem value={"null"}>null</MenuItem>
+            <MenuItem value={"Ed25519Signature2020"}>
+              Ed25519Signature2020
+            </MenuItem>
+            <MenuItem value={"null"}>
+              null
+            </MenuItem>
           </Select>
         </FormControl>
           
         <FormControl>  
-          <FormLabel className="formLabel">Serialization Type</FormLabel>
+          <FormLabel className="formLabel">
+            Serialization Type
+          </FormLabel>
           <Select
             name="serializationType"
             value={formState.serializationType}
             onChange={handleChange}
           >
-            <MenuItem value={"JSON-LD"}>JSON-LD</MenuItem>
-            <MenuItem value={"null"}>null</MenuItem>
+            <MenuItem value={"JSON-LD"}>
+              JSON-LD
+            </MenuItem>
+            <MenuItem value={"null"}>
+              null
+            </MenuItem>
           </Select>
         </FormControl>
       </Box>
