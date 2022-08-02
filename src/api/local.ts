63b4b73bdacf5101bc44adb5c046c203ed18854f
Gabe Fields <20584379+fields37@users.noreply.github.com>
2022-08-01 23:13:33 -0400

    some progress on building out verification results display

--- src/api/local.ts
+++ src/api/local.ts
@@ -8,21 +8,15 @@ export interface IssueParams {
 
 export function SignCredential(unsignedCredential: object, options: IssueParams)
 {
-	console.log(unsignedCredential);
-	console.log(options);
 	return {hello: "world"};
 }
 
 export function VerifyCredential(unVerifiedCredential: object)
 {
-	var expiration = Math.random() < 0.5;
-	var valid_signature = Math.random() < 0.5;
-	var issuer_did_resolves = Math.random() < 0.5;
-	var revocation_status = Math.random() < 0.5;
 	return [
-		{id: "expiration", valid: expiration},
-		{id: "valid_signature", valid: valid_signature},
-		{id: "issuer_did_resolves", valid: issuer_did_resolves},
-		{id: "revocation_status", valid: revocation_status},
+		{id: "expiration", valid: Math.random() < 0.5},
+		{id: "valid_signature", valid: Math.random() < 0.5},
+		{id: "issuer_did_resolves", valid: Math.random() < 0.5},
+		{id: "revocation_status", valid: Math.random() < 0.5},
 	]
 }
--- src/components/VerificationResultsCard.tsx
+++ src/components/VerificationResultsCard.tsx
@@ -1,12 +1,19 @@
 import { VerificationResultsProps } from "../components/Props";
 import {
-	Button,
+	Alert,
+	AlertTitle,
 	Box,
 	Card,
-	Snackbar,
+	CardActionArea,
+	CardContent,
+	Divider,
+	Grid,
+	Typography,
  } from '@mui/material'
 import { useState, FC } from 'react'
 import React from 'react'
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 // TODO: Create props type that takes in verification results array of key value pairs
 // TODO: Format 3 sections with checks and errors, divider between first and second section
@@ -24,20 +31,86 @@ export const VerificationResultsCard: FC<VerificationResultsProps> = ({
 		);
 
 	return (
-		<Card>
-			{verificationSuccess && <h1>Verifification Success</h1>}
-			{!verificationSuccess && <h1>Verifification Failed</h1>}
-			{error &&
-				<h2>{error.name} : {error.message}</h2>
-			}
-			{!error &&
-				<Box>
-					<h2>{results[0].id}: {results[0].valid ? "true": "false"}</h2>
-					<h2>{results[1].id}: {results[1].valid ? "true": "false"}</h2>
-					<h2>{results[2].id}: {results[2].valid ? "true": "false"}</h2>
-					<h2>{results[3].id}: {results[3].valid ? "true": "false"}</h2>
-				</Box>
-			}
+		<Card
+			sx={{
+				width: "100%",
+				mt: "1.5rem",
+			}}
+		>
+			<CardContent>
+				<Grid container spacing={2}>
+					<Grid item xs={3}>
+						<Alert
+							severity={verificationSuccess ? "success" : "error"}
+							icon={false}
+							sx={{
+								// TODO: store this color in consts!!!
+								width: "100%",
+								height: "100%",
+								display: 'flex',
+								alignItems: 'center',
+								justifyContent: 'center',
+							}}
+						>
+							<Typography
+								align="center"
+								variant="h3"
+								sx={{
+									mx: "0.5rem",
+								}}
+							>
+								{verificationSuccess && <CheckCircleIcon
+								fontSize="large"
+								sx={{
+									color: "green",
+									mb: "-10px",
+									mr: "8px",
+								}}
+								/>}
+
+								{!verificationSuccess && <CancelIcon
+								fontSize="large"
+								sx={{
+									color: "red",
+									mb: "-10px",
+									mr: "8px",
+								}}
+								/>}
+								{!verificationSuccess && "Verification Failed"}
+								{verificationSuccess && "Verification Success"}
+							</Typography>
+						</Alert>
+					</Grid>
+					<Grid item xs={0.1}><Divider orientation="vertical"/></Grid>
+					<Grid item xs={8.5}>
+						<Alert
+							severity="error"
+							icon={false}
+							sx={{
+								height:"100%",
+								width:"100%",
+								backgroundColor: "#f3f4f6",
+							}}
+						>
+							{error &&
+								<Box>
+									<AlertTitle><strong>{error.name}</strong></AlertTitle>
+									{error.message}
+								</Box>
+							}
+
+							{!error &&
+								<Box>
+									<h2>{results[0].id}: {results[0].valid ? "true": "false"}</h2>
+									<h2>{results[1].id}: {results[1].valid ? "true": "false"}</h2>
+									<h2>{results[2].id}: {results[2].valid ? "true": "false"}</h2>
+									<h2>{results[3].id}: {results[3].valid ? "true": "false"}</h2>
+								</Box>
+							}
+						</Alert>
+					</Grid>
+				</Grid>
+			</CardContent>
 		</Card>
 	)
 };
