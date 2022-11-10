47608d249dffce418269eef99d93f2842e6a2b97
Gabe Fields <20584379+fields37@users.noreply.github.com>
2022-11-10 13:39:52 -0500

    Added styling to QR modal based off WebVerifier

--- src/components/ScanModal.tsx
+++ src/components/ScanModal.tsx
@@ -1,13 +1,16 @@
+// This component contains both the button to open the QR code modal, and the modal itself
+
 import React from 'react';
 import { QrReader } from 'react-qr-reader';
-import { useState } from 'react';
 import { Result } from '@zxing/library';
 import {
   Box,
   Button,
+  IconButton,
   Modal,
   Typography,
 } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
 
 type PropsType = {
   onScan: (result: string) => void;
@@ -41,7 +44,9 @@ export const ScanModal = ({ onScan, setErrorMessage }: PropsType) => {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: '40%',
+    // width: '40%',
+    maxWidth: '600px',
+    width: '95%',
     bgcolor: 'background.paper',
     borderRadius: '10px',
     boxShadow: 24,
@@ -51,7 +56,11 @@ export const ScanModal = ({ onScan, setErrorMessage }: PropsType) => {
 
   return (
     <Box>
+
+      {/* This is the button to open the modal */}
       <Button onClick={handleOpen}>Scan a QR code</Button>
+      
+      {/* The modal window itself */}
       <Modal
         open={open}
         onClose={handleClose}
@@ -59,6 +68,28 @@ export const ScanModal = ({ onScan, setErrorMessage }: PropsType) => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
+          {/* Modal header bar */}
+          <Box sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            flexGrow: 1,
+            alignItems: "baseline",
+            mb: "-25px",
+          }}>
+            <Typography variant="h2">
+              Scan a QR Code
+            </Typography>
+            {/* X button to close modal */}
+            <IconButton
+            onClick={closeModal}
+            sx={{left:0,}}
+            >
+              <CloseIcon color="info" fontSize="medium"/>
+            </IconButton>
+
+          </Box>
+
+          {/* QR code scanner */}
           <QrReader 
             onResult={(result, error) => handleScan(result, error)}
             constraints={{facingMode: "environment"}}
