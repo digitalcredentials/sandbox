bd0195ca3b3189f7165ad7fde7de4357fe395351
Gabe Fields <20584379+fields37@users.noreply.github.com>
2023-01-12 18:12:52 -0500

    Set download filename to date-time UTC

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -20,7 +20,9 @@ const DownloadButton = ({value}: PropsType) => {
     const element = document.createElement("a");
     const file = new Blob([value], {type: 'text/plain'});
     element.href = URL.createObjectURL(file);
-    element.download = "credential.json";
+    const event = new Date();
+    const jsonDate = event.toJSON();
+    element.download = jsonDate + ".json";
     document.body.appendChild(element); // Required for this to work in FireFox
     element.click();
     setOpen(true);
